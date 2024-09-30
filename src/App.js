3f25f186166ee583de4205a18b2b52b8b815a2be
Nadil97 <87482209+Nadil97@users.noreply.github.com>
2024-09-30 17:28:05 +0530

    imge import and css part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import HomeImage from'./assets/test.jpeg';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <img className='image-style' src={HomeImage} alt='Show this image'></img>
+        <div className='text-style'>This test use for css text</div>
+      </div>
+
   );
 }
 
