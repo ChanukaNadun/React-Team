3e4a0c774333f5014808bfa8a74aefbaebbcd432
ChanuNadun <chanukanadun07@gmail.com>
2024-10-07 17:42:14 +0530

    Uses of useState 2

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import { useState } from 'react';
 function App() {
   const [isClicked, setIsClicked] = useState(false);
 
+  let [count, setCount] = useState(0);
+
 function handelSubmit(){
   if(isClicked == false){
     setIsClicked(true);
   }else{
     setIsClicked(false);
   }
-  
+  setCount(count = count + 1 );
 }
 
   return (
     <div className="App">
-      {isClicked && <div>clicked button</div>}
-      {!isClicked && <div>click again</div>}
+      
+      {isClicked ? (<div>Yes {count}</div>) : (<div>No {count}</div>)}
+
       <button onClick={handelSubmit}>Click Here</button>
+      
     </div>
   );
 }
